187ee235fb2e9abe194c1f04db3388bcf4a3adea
Chris Zetter <zetter@gmail.com>
2014-02-03 21:01:44 +0000

    Tied them together like a boss

---
+++ simple.rb
@@ -0,0 +1,15 @@
+require 'treetop'
+require 'atom'
+require 'list'
+require 'evaluator'
+
+
+
+Treetop.load('scheme.treetop')
+
+parser = SchemeParser.new
+
+program = parser.parse("(car (cdr l))").to_ast
+l = parser.parse("(1 2 3)").to_ast
+
+p Evaluator.new(program).evaluate({"l" => l})
